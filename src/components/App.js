b58ba6b8bc1ec8e2a6307b76e7bde59f8a95e820
NodeStake <info@nodestake.top>
2023-02-09 19:24:24 +0800

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -814,9 +814,9 @@ class App extends React.Component {
           <a href="https://twitter.com/NodeStake_top" target="_blank" rel="noreferrer" className="col-md-4 mb-0 text-muted">
             {this.props.theme === 'light'
               ? (
-                <img src={TooltipIcon} alt="Twitter" width={0} />
+                <img src={TooltipIcon} alt="Twitter" width={10} />
               ) : (
-                <img src={TooltipIcon} alt="Twitter" width={0} />
+                <img src={TooltipIcon} alt="Twitter" width={10} />
               )}
           </a>
 
